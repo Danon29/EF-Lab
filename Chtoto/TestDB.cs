f1dd1f02f05394c53ab006e246d1d523963ec89a
usman <usman@DESKTOP-D2H8SGN>
2021-08-10 09:42:03 +0200

    Добавьте файлы проекта.

---
+++ Chtoto/TestDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chtoto
+{
+    public class TestDB : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Server= desktop-d2h8sgn; Database= Chtoto; Trusted_Connection = True ;");
+        }
+    }
+}
